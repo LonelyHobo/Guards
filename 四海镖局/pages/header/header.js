25b65eae13a02214e61f246bf687e1103a9f973c
LonelyHobo <1144646550@qq.com>
2019-05-04 21:53:05 +0800

    11

---
+++ 四海镖局/pages/header/header.js
@@ -0,0 +1,19 @@
+//header.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    headertitle: '四海镖局',
+  },
+  //事件处理函数
+  bindViewTap: function () {
+   
+  },
+  onLoad: function () {
+    
+  },
+  getUserInfo: function (e) {
+    
+  }
+})
